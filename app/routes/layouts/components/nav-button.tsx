366169f8f501f7e2d2ef199eec9ec03005acedde
jaeon <jae1321@naver.com>
2025-05-09 11:26:45 +0900

    비즈니스 고객제보 구현

---
+++ app/routes/layouts/components/nav-button.tsx
@@ -0,0 +1,16 @@
+import { Button } from '~/components/ui/button';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export default function NavButton({ children }: Props) {
+  return (
+    <Button
+      variant="ghost"
+      className="text-[rgb(78, 89, 104)] text-[15px] leading-[1.5] font-[500] hover:text-[#318216]"
+    >
+      {children ?? ''}
+    </Button>
+  );
+}
---
+++ app/routes/pages/home/components/business-call.tsx
@@ -0,0 +1,26 @@
+import { Button } from '~/components/ui/button';
+
+export default function BusinessCall() {
+  return (
+    <section className="h-[306px] py-[33px]">
+      <div className="container flex items-center justify-around">
+        <div>
+          <h2 className="text-[36px] leading-[1.3] font-[700] text-[#333d4b]">
+            비즈니스 고객 제보
+          </h2>
+          <p
+            className="[leading-[1.5] text-[18px] font-[400]"
+            style={{ color: 'rgb(0, 19, 43, 0.58)' }}
+          >
+            토스팀과의 협업 중 불편함을 느끼셨나요? <br />
+            지금 바로 알려주세요.
+          </p>
+          <Button variant="secondary">접수하기</Button>
+        </div>
+        <div className="w-[240px]">
+          <img src="/images/business-call.png" alt="business-call" />
+        </div>
+      </div>
+    </section>
+  );
+}
