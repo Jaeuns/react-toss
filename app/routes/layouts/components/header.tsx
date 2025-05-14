673f7b41ce829b4b89f4ffcc4efe8e6ded52a96a
jaeon <jae1321@naver.com>
2025-05-14 12:24:23 +0900

    반응형 container 적용 및 헤더 스크롤 그림자선 추가

--- app/routes/layouts/components/header.tsx
+++ app/routes/layouts/components/header.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router';
 
+import Hamburger from '~/components/svg/hamburger.svg?react';
 import TossLogo from '~/components/svg/toss-logo.svg?react';
+import { Button } from '~/components/ui/button';
 import { useLanguage } from '~/hooks/use-language';
+import useMediaQuery, { MediaQuery } from '~/hooks/use-media-query';
+import { cn } from '~/lib/utils';
 
 import LangButton from './lang-button';
 import NavButton from './nav-button';
@@ -30,7 +35,7 @@ const MENU: NavItem[] = [
   },
   {
     to: '#',
-    label: '토스 인증서',
+    label: '토스인증서',
   },
   {
     to: '#',
@@ -40,29 +45,65 @@ const MENU: NavItem[] = [
 
 export default function Header() {
   const [language, setLanguage] = useLanguage();
+  const isMobile = useMediaQuery(MediaQuery.MOBILE);
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
+  // ? Debug
+  useEffect(() => {
+    console.log('isMobile', isMobile);
+  }, [isMobile]);
 
   return (
-    <header className="fixed z-50 h-[60px] w-full bg-white">
+    <header
+      className={cn('fixed z-50 h-[60px] w-full bg-white', scrolled && 'shadow-sm')}
+    >
       <div className="container flex h-full items-center justify-between">
         <Link to="/" className="w-[66px]">
           <TossLogo />
         </Link>
-        <nav className="flex gap-4">
-          {MENU.map((item, i) => (
-            <Link to={item.to} key={i}>
-              <NavButton>{item.label}</NavButton>
-            </Link>
-          ))}
-        </nav>
-        <div className="flex items-center gap-2">
-          <LangButton language={language} setLanguage={setLanguage} value="ko">
-            KOR
-          </LangButton>
-          <span className="text-[#d1d6db]">|</span>
-          <LangButton language={language} setLanguage={setLanguage} value="en">
-            ENG
-          </LangButton>
-        </div>
+        {isMobile ? (
+          <div>
+            <Button>앱 다운로드</Button>
+            <Button variant="ghost">
+              <Hamburger className="h-6 w-6" />
+            </Button>
+          </div>
+        ) : (
+          <>
+            <nav className="flex gap-4">
+              {MENU.map((item, i) => (
+                <Link to={item.to} key={i}>
+                  <NavButton>{item.label}</NavButton>
+                </Link>
+              ))}
+            </nav>
+            <div>
+              <LangButton language={language} setLanguage={setLanguage} value="ko">
+                KOR
+              </LangButton>
+              <span className="text-[#d1d6db]">|</span>
+              <LangButton language={language} setLanguage={setLanguage} value="en">
+                ENG
+              </LangButton>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
